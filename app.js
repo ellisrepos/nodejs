a883bd85b81015f30c389f50f8a3ed318bafe5da
Ellis Sentoso <ellis95officialgmail.com@Elliss-MacBook-Air.local>
2026-01-15 10:56:33 -0800

    express

--- app.js
+++ app.js
@@ -2,4 +2,25 @@ const express = require('express');
 // express app
 const app = express();
 // listen for request
-app.listen(8080)
+app.listen(8080);
+app.get('/', (req, res) => {
+    // How to send the response
+    // res.write() 
+    // res.end()
+    // res.send('<p>Home page</p>');
+    res.sendFile('./views/index.html', {root: __dirname})
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname})
+});
+
+// redirects
+app.get('/about-me', (req, res) => {
+    res.redirect('/about');
+})
+
+// 404 - create middleware and fires for every request that's invalid
+app.use((req, res) => {
+    res.sendFile('./views/404.html', {root: __dirname})
+})
